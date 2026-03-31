ff5bd12e070ab3d7ecdef50935eac9b955ba6b96
Bardcastle <bardcastle2@gmail.com>
2026-03-31 14:44:10 -0400

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public bool GameOver = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
